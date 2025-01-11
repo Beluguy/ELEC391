7f199f6c6bd7a5ede814d360ceca028f7b3bd139
Beluguy <hello@marcusc.me>
2025-01-10 20:34:29 -0800

    added the Angle sensing starter assignment

---
+++ Angle sensing starter assignment/Task_1/task1/task1.ino
@@ -0,0 +1,20 @@
+#include "Arduino_BMI270_BMM150.h"
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+ if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+}
+
+void loop() {
+  delay (100);
+  float x, y, z;
+
+  if (IMU.gyroscopeAvailable()) {
+    IMU.readGyroscope(x, y, z);
+    Serial.println(x);
+  }
+}
