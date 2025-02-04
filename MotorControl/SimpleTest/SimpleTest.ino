06293e425b5a585ebbb7b6d237a94ffae9ffefd9
ryang <ryang.kim1204@gmail.com>
2025-02-04 10:48:04 -0800

    edited pin out define, works now

--- MotorControl/SimpleTest/SimpleTest.ino
+++ MotorControl/SimpleTest/SimpleTest.ino
@@ -1,17 +1,27 @@
+  #define PWM_1 A7
+  #define PWM_2 A6
+  #define PWM_3 A5
+  #define PWM_4 A4
+
+
 void setup() {
   // put your setup code here, to run once:
 
-  #define PWM_RIGHT 24
-  #define PWM_LEFT 23
 
-  pinMode(PWM_RIGHT, OUTPUT);
-  pinMode(PWM_LEFT, OUTPUT);
+
+  pinMode(PWM_1, OUTPUT);
+  pinMode(PWM_2, OUTPUT);
+  pinMode(PWM_3, OUTPUT);
+  pinMode(PWM_4, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  analogWrite(PWM_RIGHT, 127);
-  analogWrite(PWM_LEFT,127);
+  analogWrite(PWM_1, 0);
+  analogWrite(PWM_2, 77);
+  analogWrite(PWM_3, 127);
+  analogWrite(PWM_4, 255);
+
 }
