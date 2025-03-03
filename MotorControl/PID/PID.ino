0676e83b6a7f123940c0ff0f9f4fb0b4d22eede2
ryang <ryang.kim1204@gmail.com>
2025-03-02 18:13:31 -0800

    pid controller base code draft (no real numbers)

---
+++ MotorControl/PID/PID.ino
@@ -0,0 +1,94 @@
+#include "Arduino_BMI270_BMM150.h"
+#include "mbed.h"
+
+//MOTOR 1
+#define M1F D7 //BIN1 - GREEN
+#define M1B D8 //BIN2 - BLUE
+//MOTOR 2
+#define M2F D10 //AIN1 - BLUE
+#define M2B D9 //AIN2 - GREEN
+
+float SampleRate;
+float currentAngle = 0;
+
+float accX, accY, accZ, gyroX, gyroY, gyroZ, kAcc, kGyro, accAngle, gyroAngle, errorAngle, targetAngle;
+float Kp, Ki, Kd;
+
+void setup() {
+
+  Serial.begin(9600);
+  while (!Serial);
+ if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+  SampleRate = IMU.gyroscopeSampleRate();
+
+  pinMode(M1F, OUTPUT);
+  pinMode(M1B, OUTPUT);
+  pinMode(M2F, OUTPUT);
+  pinMode(M2B, OUTPUT);
+}
+
+void loop() {
+
+  //Complementary filter constants
+  kAcc = 0.05;
+  kGyro = 0.95;
+
+  targetAngle = 0;
+
+  if (IMU.gyroscopeAvailable()&&IMU.accelerationAvailable()) {
+    IMU.readAcceleration(accX, accY, accZ);
+    accAngle = RAD_TO_DEG*(atan(accY/accZ));
+
+    IMU.readGyroscope(gyroX, gyroY, gyroZ);
+
+    gyroAngle = (1.00/SampleRate)*gyroX;
+
+    currentAngle = kGyro*(gyroAngle + currentAngle) + kAcc*(accAngle);
+    Serial.println(currentAngle);
+  }
+
+  //PID
+
+  Kp = ;
+  Ki = ;
+  Kd = ;
+
+  pError = targetAngle - currentAngle;
+  iError += pError*(1.0/SampleRate); 
+  dError = (pError - prevError)/(1.0/SampleRate);
+
+  prevError = pError;
+
+  motorControlVar = Kp*pError + Ki*iError + Kd*dError;
+
+  if(errorAngle < -2) {
+
+    analogWrite(M1F, 255);
+    analogWrite(M1B, 255-round(255*abs(errorAngle)/90));
+
+    analogWrite(M2F, 255);
+    analogWrite(M2B, 255-round(255*abs(errorAngle)/90));
+
+  } else if(errorAngle > 2) {
+
+    analogWrite(M1F, 255-round(255*abs(errorAngle)/90));
+    analogWrite(M1B, 255);
+
+    analogWrite(M2F, 255-round(255*abs(errorAngle)/90));
+    analogWrite(M2B, 255);
+
+
+  } else {
+    analogWrite(M1F, 0);
+    analogWrite(M1B, 0);
+
+    analogWrite(M2F, 0);
+    analogWrite(M2B, 0);
+    
+  }
+
+  //delay(100);
+}
