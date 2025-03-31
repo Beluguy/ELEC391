33536a2262094219da4eb1ee8b095da740ecbd9c
Marcus <hello@marcusc.me>
2025-03-31 16:51:17 -0700

    :)

--- Self-balancing-robot/bluetooth_pid_diy_turning/bluetooth_pid_diy_turning.ino
+++ Self-balancing-robot/bluetooth_pid_diy_turning/bluetooth_pid_diy_turning.ino
@@ -2,7 +2,7 @@
 #include "Arduino_BMI270_BMM150.h"  // IMU
 #include "mbed.h"                   // Customer PWM freq
 
-#define BUFFER_SIZE 20
+#define BUFFER_SIZE 1
 #define PWM_FREQ 10000.0
 
 #define M1F D10 //Green: motor 1
@@ -17,18 +17,18 @@ mbed::PwmOut M1BPin(digitalPinToPinName(M1B));
 mbed::PwmOut M2FPin(digitalPinToPinName(M2F));
 
 //--------------------------PID----------------------------------------------------------
-float Kp = 0.0, Ki = 0.0, Kd = 0.0;
+float Kp = 80.0, Ki = 1000.0, Kd = 1.5;
 float pOut = 0.0, iOut = 0.0, dOut = 0.0;
-float currentAngle = 0.0, lastAngle = 0.0, targetAngle = 0.0, currPWM = 0.0, lastPWM = 0.0, currError = 0.0, lastError = 0.0, dt;
+float currentAngle = 0.0, lastAngle = 0.0, targetAngle = 0.0, currPWM = 0.0, lastPWM = 0.0, currError = 0.0, lastError = 0.0, dt, bias = 0.0;
 //---------------------------------------------------------------------------------------
 
 //-------------Comp Angle-------------------------------------------
 float accX = 0.0, accY = 0.0, accZ = 0.0, gyroX = 0.0, gyroY = 0.0, gyroZ = 0.0, kAcc = 0.05, kGyro = 0.95;
 double accAngle, gyroAngle;
 //-------------------------------------------------------------------
-int turn = 0, lastTurn = 0;
+int turn = 0; // 0 = balance, 1 = forward, 2 = left, 3 = right, 4 = backward
 unsigned long loopTime, lastBLECheck = 0;
-bool isConnected, newDataReceived;
+bool isConnected;
 
 // Define a custom BLE service and characteristic
 BLEService customService("fc096266-ad93-482d-928c-c2560ea93a4e");
@@ -106,17 +106,25 @@ void loop() {
       if (customCharacteristic.written()) {
       int length = customCharacteristic.valueLength();
       
-        if (length == 13) { // Expecting 20 bytes (5 floats)
-          static uint8_t data[13];
+        if (length == 1) {
+          static uint8_t data[1];
           customCharacteristic.readValue(data, length);
+          memcpy(&turn, data, 1);
 
-          memcpy(&Kp, data + 1, 4); // Extract third float
-          memcpy(&Ki, data + 5, 4); // Extract fourth float
-          memcpy(&Kd, data + 9, 4); // Extract fifth float
+          // if (turn == 1) targetAngle += 0.2;
+          // else if (turn == 4) targetAngle -= 0.2;
+          // else if (!turn) targetAngle = 0.0;
+
+          if (turn == 1) bias += 5.0;
+          else if (turn == 4) bias -= 5.0;
+          else if (!turn) bias = 0.0;
+          // Serial.print(turn);
+          // Serial.print("\t");
+          // Serial.println(targetAngle);
         }
       }
     } else {
-      if(isConnected) {
+      if (isConnected) {
         isConnected = false; 
         digitalWrite(LED_BUILTIN, LOW); // Turn off LED when disconnected
       }
@@ -125,28 +133,46 @@ void loop() {
   //-----------------------------------------------------------------------
 
   //----------------complementary filter------------------------
-  if (IMU.accelerationAvailable()) {
-    IMU.readAcceleration(accX, accY, accZ);
-    accAngle = RAD_TO_DEG * atan(accY/accZ);
+  // if (IMU.accelerationAvailable()) {
+  //   IMU.readAcceleration(accX, accY, accZ);
+  //   accAngle = RAD_TO_DEG * atan(accY/accZ);
+  // }
+
+  // dt = (micros() - lastIMUTime) / 1000000.0;
+  // lastIMUTime = micros();
+  // //Serial.println(dt,6);
+  // //Serial.print("\t");
+
+  // if (IMU.gyroscopeAvailable()) {
+  //   IMU.readGyroscope(gyroX, gyroY, gyroZ);
+  //   gyroAngle = -1.0 * gyroX * dt + currentAngle;
+  // } 
+
+  // currentAngle = kGyro * gyroAngle + kAcc * accAngle ;
+
+  if (IMU.readAcceleration(accX, accY, accZ) && IMU.readGyroscope(gyroX, gyroY, gyroZ)) {
+    static unsigned long lastTime = micros();
+    dt = (micros() - lastTime) / 1000000.0;
+    lastTime = micros();
+    
+    //Serial.println(dt);
+
+    if(accZ < 0.1){
+      accAngle = 0.0;
+      gyroAngle = 0.0;
+    } else {
+      accAngle = RAD_TO_DEG*atan(accY/accZ);
+      gyroAngle = -1.0 * gyroX * dt + currentAngle;
+    }
+
+    currentAngle = kGyro*(gyroAngle) + kAcc*(accAngle);
+    // Serial.print("Current Angle: ");
+    // Serial.print(currentAngle);
+    // Serial.print("\t");
+    // Serial.print(gyroAngle);
+    // Serial.print("\t");
+    // Serial.println(accAngle);
   }
-
-  dt = (micros() - lastIMUTime) / 1000000.0;
-  lastIMUTime = micros();
-  //Serial.println(dt,6);
-  //Serial.print("\t");
-
-  if (IMU.gyroscopeAvailable()) {
-    IMU.readGyroscope(gyroX, gyroY, gyroZ);
-    gyroAngle = -1.0 * gyroX * dt + currentAngle;
-  } 
-
-  currentAngle = kGyro * gyroAngle + kAcc * accAngle ;
-  // Serial.print("Current Angle: ");
-  // Serial.print(currentAngle);
-  // Serial.print("\tgyroAngle: ");
-  // Serial.print(gyroAngle);
-  // Serial.print("\taccAngle: ");
-  // Serial.println(accAngle);
   //-----------------------------------------------------------
 
   //----------------------PID---------------------------------
@@ -162,7 +188,7 @@ void loop() {
   remainingMin = -1000.0 - (pOut + dOut);             // clamp integral windup
   iOut = constrain(iOut, remainingMin, remainingMax);  
 
-  currPWM = pOut + dOut + iOut;
+  currPWM = bias + pOut + dOut + iOut;
   currPWM = constrain(currPWM, -1000.0, 1000.0);
   // Serial.print("\t");
   // Serial.print(currPWM,5);
@@ -199,7 +225,5 @@ void loop() {
   // Serial.print(Kd,5);
   // Serial.print("\t");
   // Serial.println(speed,5);
-  // Serial.print(lastTurn);
   // Serial.print("\t");
-  // Serial.println(turn);
 }
